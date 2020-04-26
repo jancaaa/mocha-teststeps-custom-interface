73d595ee55d4212c85a02b40f2ccb6c31be8f342
Jana <janca.zahradnickova@gmail.com>
2020-04-26 15:12:27 +0200

    example tests

--- test/example-nonparam.js
+++ test/example-nonparam.js
@@ -4,39 +4,58 @@ beforeEachSuite(function () {
 	console.log('This function will run before each suite.');
 });
 
+beforeEachSuite(function () {
+	console.log('You can have more hooks of the same type. They will run in order of creation.');
+});
+
 afterEachSuite(function () {
 	console.log('This function will run after each suite.');
 });
 
 mysuite('SUITE1', function () {
 	beforeEachTest(function () {
-		console.log('This function will run before each test in the suite. For correct behavior it must be defined in fn of mysuite()');
+		console.log('This function will run before each test in SUITE1. For correct behaviour beforeEachTest() must be inside mysuite().');
+		console.log('You can access the test config (or other test parameters) from beforeEachTest()/beforeTest()/afterEachTest()/afterTest() hooks: ' + this.test.parent.config);
 	});
 
 	afterEachTest(function () {
-		console.log('This function will run after each test in the suite. For correct behavior it must be defined in fn of mysuite()');
+		console.log('This function will run after each test in SUITE1. For correct behavior afterEachTest() must be inside mysuite().');
 	});
 
 	mytest('TEST1', 'ID1', function () {
+		beforeTest(function () {
+			console.log('This function will run once - before first step of the test');
+		});
+
+		afterTest(function () {
+			console.log('This function will run once - after last step of the test. It runs also in case of failure of a step.');
+		});
+
 		step('Step11', 'Result11', function () {
+			console.log('This step will pass.');
 			expect(true).to.be.true;
 		});
 		step('Step12', 'Result12', function () {
+			console.log('This step will pass.');
 			expect(true).to.be.true;
 		});
 		step('Step13', 'Result13', function () {
+			console.log('This step will pass.');
 			expect(true).to.be.true;
 		});
 	});
 
 	mytest('TEST2', 'ID2', function () {
 		step('Step21', 'Result21', function () {
+			console.log('This step will pass.');
 			expect(true).to.be.true;
 		});
 		step('Step22', 'Result22', function () {
-			expect(true).to.be.true;
+			console.log('This step will fail, so the rest of the steps will be skipped.');
+			expect(true).to.be.false;
 		});
 		step('Step23', 'Result23', function () {
+			console.log('This step will be skipped. If you can see this in the console something went wrong.');
 			expect(true).to.be.true;
 		});
 	});
--- test/example-param.js
+++ test/example-param.js
@@ -10,40 +10,51 @@ afterEachSuite(function () {
 
 mysuite('SUITE1', function () {
 	beforeEachTest(function () {
-		console.log('This function will run before each test in the suite. For correct behavior it must be defined in fn of mysuite()');
+		console.log('This function will run before each test in SUITE1. For correct behaviour beforeEachTest() must be inside mysuite().');
+		console.log('You can access the test config (or other test parameters) from beforeEachTest()/beforeTest()/afterEachTest()/afterTest() hooks: ' + this.test.parent.config);
 	});
 
 	afterEachTest(function () {
-		console.log('This function will run after each test in the suite. For correct behavior it must be defined in fn of mysuite()');
+		console.log('This function will run after each test in SUITE1. For correct behavior afterEachTest() must be inside mysuite().');
 	});
 
-	testParam('TEST1', 'ID1', [1, 2], function () {
+	testParam('TEST1', 'ID1', ['configuration1', 'configuration2'], function () {
+		beforeTest(function () {
+			console.log('This function will run before each configuration of TEST1.');
+		});
+
 		step('Step11', 'Result11', function () {
+			console.log('This step will pass.');
 			expect(true).to.be.true;
 		});
 		step('Step12', 'Result12', function () {
+			console.log('This step will pass.');
 			expect(true).to.be.true;
 		});
 		step('Step13', 'Result13', function () {
+			console.log('This step will pass.');
 			expect(true).to.be.true;
 		});
 	});
 
-	testParam('TEST2', 'ID2', [1, 2], function () {
+	testParam('TEST2', 'ID2', ['configuration1', 'configuration2'], function () {
 		step('Step21', 'Result21', function () {
+			console.log('This step will pass.');
 			expect(true).to.be.true;
 		});
 		step('Step22', 'Result22', function () {
-			expect(true).to.be.true;
+			console.log('This step will fail, so the rest of the steps will be skipped.');
+			expect(true).to.be.false;
 		});
 		step('Step23', 'Result23', function () {
+			console.log('This step will be skipped. If you can see this in the console something went wrong.');
 			expect(true).to.be.true;
 		});
 	});
 });
 
 mysuite('SUITE2', function () {
-	testParam('TEST3', 'ID3', [1, 2], function () {
+	testParam('TEST3', 'ID3', ['configuration1', 'configuration2'], function () {
 		step('Step31', 'Result31', function () {
 			expect(true).to.be.true;
 		});
@@ -55,7 +66,7 @@ mysuite('SUITE2', function () {
 		});
 	});
 
-	testParam('TEST4', 'ID4', [1, 2], function () {
+	testParam('TEST4', 'ID4', ['configuration1', 'configuration2'], function () {
 		step('Step41', 'Result41', function () {
 			expect(true).to.be.true;
 		});
